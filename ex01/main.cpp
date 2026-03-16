1dd97e3bd17ba91ccbd9d965160b37e60675008f
OKLMARO <mounioma@gmail.com>
2026-03-16 16:49:16 +0100

    v2.1 ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 14:07:30 by oamairi           #+#    #+#             */
-/*   Updated: 2026/03/16 14:33:44 by oamairi          ###   ########.fr       */
+/*   Updated: 2026/03/16 16:43:43 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,20 @@ int main()
 {
 	Animal* j = new Dog();
 	Animal* i = new Cat();
+	Cat* cat1 = new Cat();
+	Cat* cat2 = new Cat(*cat1);
+	Cat* cat3 = cat1;
+	cat1->setIdea("Je suis cool", 0);
+	cat2->setIdea("Je suis cool", 1);
 	j->setIdea("Je suis cool", 0);
 	j->setIdea("Je suis beau", 1);
 	i->setIdea("Je suis mignon", 2);
 	std::cout << i->getType() << " a dit: " << i->getIdea(2) << "\n";
 	std::cout << j->getType() << " a dit: " << j->getIdea(0) << "\n";
 	std::cout << j->getType() << " a dit: " << j->getIdea(1) << "\n";
+	std::cout << cat1->getType() << " a dit: " << cat1->getIdea(1) << "\n";
+	std::cout << cat2->getType() << " a dit: " << cat2->getIdea(1) << "\n";
+	std::cout << cat3->getType() << " a dit: " << cat3->getIdea(1) << "\n";
 	delete j;
 	delete i;
 	return 0;
